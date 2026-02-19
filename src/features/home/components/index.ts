fe85c900f355aab24dd3ab005039cd2439171d77
Boban094 <boban.mihajlovic1994@gmail.com>
2026-02-19 18:25:10 +0100

    [03-notification-fix] - fix za notifikacije

--- src/features/home/components/index.ts
+++ src/features/home/components/index.ts
@@ -1,4 +1,3 @@
 export { WelcomeCard } from './WelcomeCard';
 export { QuickStats } from './QuickStats';
 export { ActiveDelivery } from './ActiveDelivery';
-export { NotificationPrompt } from './NotificationPrompt';
